37e94050d71c32e9f5f1de480964a05d96bd08d0
artinex <jonathannb123@hotmail.com>
2022-01-18 15:23:55 -0400

    ff

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
-class ModelTest(TestCase):
+class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating new user with good email"""
@@ -15,4 +15,3 @@ class ModelTest(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-        
