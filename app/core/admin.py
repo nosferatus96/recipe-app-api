439d9dc0aef2723489d94ff8cbb0b7623a55d68d
artinex <jonathannb123@hotmail.com>
2022-01-18 18:06:16 -0400

    ddd

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,12 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import ModelTests
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
