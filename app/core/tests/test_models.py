e99039188f8d762a3dbd7d77c595b1fe8dd02dcd
artinex <jonathannb123@hotmail.com>
2022-01-18 16:11:18 -0400

    iii

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -27,3 +27,13 @@ class ModelTests(TestCase):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'totop900')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'jonathannb123@hotmail.com',
+            'totop900'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
