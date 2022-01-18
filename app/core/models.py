5d007d65ef05d6d82bf93a30a17e38aa52d83918
artinex <jonathannb123@hotmail.com>
2022-01-18 16:15:45 -0400

    uu

--- app/core/models.py
+++ app/core/models.py
@@ -14,6 +14,15 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password):
+        """Creates and saves a new new user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Custim user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
