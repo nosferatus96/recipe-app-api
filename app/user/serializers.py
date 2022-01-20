05978c39c2e8f74e294e9740a46e40545b83ab00
artinex <jonathannb123@hotmail.com>
2022-01-20 19:38:29 -0400

    dd

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -4,9 +4,9 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-     """Serializer for the users object"""
+    """Serializer for the users object"""
 
-     class Meta:
+    class Meta:
          model = get_user_model()
          fields = ('email', 'password', 'name')
          extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
