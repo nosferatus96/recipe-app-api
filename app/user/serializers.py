62fcda42352c10703e2f1c969e1b95bd3070db54
artinex <jonathannb123@hotmail.com>
2022-01-20 19:44:30 -0400

    JJ

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -7,9 +7,9 @@ class UserSerializer(serializers.ModelSerializer):
     """Serializer for the users object"""
 
     class Meta:
-         model = get_user_model()
-         fields = ('email', 'password', 'name')
-         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+        model = get_user_model()
+        fields = ('email', 'password', 'name')
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
         """Create new user with encrypt pw and return"""
