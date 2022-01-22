eed8c3e551887af55230a3d11134dd59d2245941
artinex <jonathannb123@hotmail.com>
2022-01-21 20:04:29 -0400

    fff

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,6 +16,16 @@ class UserSerializer(serializers.ModelSerializer):
         """Create new user with encrypt pw and return"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """update a user, setting the pw correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
         """Serializer for the user auth object"""
