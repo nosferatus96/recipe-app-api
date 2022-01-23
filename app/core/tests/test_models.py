7eccd368a6b15283cd8c727aa5b0b80cbd9e79f6
artinex <jonathannb123@hotmail.com>
2022-01-23 16:00:03 -0400

    vvv

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 from core import models
 
+
 def sample_user(email='test@hotmailcom', password='totop900'):
     """Create a sample user"""
     return get_user_model().objects.create_user(email, password)
